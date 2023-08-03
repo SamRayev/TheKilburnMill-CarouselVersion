3e68709717c6a581b83419fd8f37ce0e59790c90
SamRayev <116523650+SamRayev@users.noreply.github.com>
2023-08-03 17:57:35 -0400

    Update gallery.jsx

--- client/src/Pages/gallery.jsx
+++ client/src/Pages/gallery.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import Image from '../Components/Event/picture';
+import ImageOne from '../Assets/GalleryImages/ImageOne.jpeg';
+import ImageTwo from '../Assets/GalleryImages/ImageTwo.JPG';
+import ImageThree from '../Assets/GalleryImages/ImageThree.JPG';
+import ImageFour from '../Assets/GalleryImages/ImageFour.JPG';
+import ImageFive from '../Assets/GalleryImages/ImageFive.JPG';
+import ImageSix from '../Assets/GalleryImages/ImageSix.jpg';
 import '../Styles/Gallery.css';
 
 const Gallery = () => {
@@ -7,31 +12,12 @@ const Gallery = () => {
         <div id='gallery'>
             <h1 className='mb-2'>GALLERY</h1>
             <div className='gallery-items'>
-                <Image className="gallery-item" image={Image} />
-                <Image className="gallery-item" image={Image} />
-                <Image className="gallery-item" image={Image} />
-                <Image className="gallery-item" image={Image} />
-                <Image className="gallery-item" image={Image} />
-                <Image className="gallery-item" image={Image} />
-                <Image className="gallery-item" image={Image} />
-                <Image className="gallery-item" image={Image} />
-                <Image className="gallery-item" image={Image} />
-                <Image className="gallery-item" image={Image} />
-                <Image className="gallery-item" image={Image} />
-                <Image className="gallery-item" image={Image} />
-                <Image className="gallery-item" image={Image} />
-                <Image className="gallery-item" image={Image} />
-                <Image className="gallery-item" image={Image} />
-                <Image className="gallery-item" image={Image} />
-                <Image className="gallery-item" image={Image} />
-                <Image className="gallery-item" image={Image} />
-                <Image className="gallery-item" image={Image} />
-                <Image className="gallery-item" image={Image} />
-                <Image className="gallery-item" image={Image} />
-                <Image className="gallery-item" image={Image} />
-                <Image className="gallery-item" image={Image} />
-                <Image className="gallery-item" image={Image} />
-                <Image className="gallery-item" image={Image} />
+                <img className="gallery-item" src={ImageOne} />
+                <img className="gallery-item" src={ImageTwo} />
+                <img className="gallery-item" src={ImageThree} />
+                <img className="gallery-item" src={ImageFour} />
+                <img className="gallery-item" src={ImageFive} />
+                <img className="gallery-item" src={ImageSix} />
             </div>
         </div>
     )
