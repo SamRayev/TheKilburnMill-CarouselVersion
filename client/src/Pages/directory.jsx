7a3147e3932297410be5bdf365a0c8a9572c0013
SamRayev <116523650+SamRayev@users.noreply.github.com>
2023-08-05 14:28:04 -0400

    Update directory.jsx

--- client/src/Pages/directory.jsx
+++ client/src/Pages/directory.jsx
@@ -1,92 +1,213 @@
-import React, { useState } from "react";
+import React from "react";
+import '../Styles/Directory.css'
 
-// Component for individual rooms
-const Room = ({ id, position, size, isSelected, onSelect }) => {
-  const roomStyle = {
-    position: "absolute",
-    top: position.y,
-    left: position.x,
-    width: size.width,
-    height: size.height,
-    border: isSelected ? "2px solid red" : "1px solid black",
-    background: isSelected ? "red" : "#e8e8e8",
-    transition: isSelected ? ".1s" : ".1s",
-    cursor: "pointer",
-  };
-
-  return <div style={roomStyle} onClick={() => onSelect(id)}></div>;
-};
-
-// Component for the floor plan containing all rooms
-const FloorPlan = ({ rooms, selectedRoomId, onRoomSelect }) => {
+function Directory() {
   return (
-    <div style={{ position: "relative", width: "500px", height: "500px" }}>
-      {rooms.map((room) => (
-        <Room
-          key={room.id}
-          id={room.id}
-          position={room.position}
-          size={room.size}
-          isSelected={selectedRoomId === room.id}
-          onSelect={onRoomSelect}
-        />
-      ))}
-    </div>
-  );
-};
-
-// Component for the room list
-const RoomList = ({ rooms, onRoomSelect }) => {
-  return (
-    <ul>
-      {rooms.map((room) => (
-        <li key={room.id} onClick={() => onRoomSelect(room.id)}>
-          Room {room.id}
-        </li>
-      ))}
-    </ul>
-  );
-};
-
-const Directory = () => {
-  const [rooms, setRooms] = useState([
-    { id: 1, position: { x: 51, y: 50 }, size: { width: 40, height: 130 } },
-    { id: 2, position: { x: 90, y: 50 }, size: { width: 80, height: 140 } },
-    { id: 3, position: { x: 158, y: 50 }, size: { width: 12, height: 90 } },
-    { id: 4, position: { x: 158, y: 50 }, size: { width: 12, height: 45 } },
-    { id: 5, position: { x: 169, y: 50 }, size: { width: 12, height: 90 } },
-    { id: 6, position: { x: 169, y: 50 }, size: { width: 12, height: 45 } },
-    { id: 7, position: { x: 200, y: 50 }, size: { width: 20, height: 30 } },
-    { id: 8, position: { x: 200, y: 79 }, size: { width: 20, height: 40 } },
-    { id: 9, position: { x: 200, y: 118 }, size: { width: 20, height: 40 } },
-    { id: 10, position: { x: 200, y: 157 }, size: { width: 20, height: 20 } },
-    { id: 11, position: { x: 219, y: 50 }, size: { width: 50, height: 127 } },
-    { id: 12, position: { x: 268, y: 50 }, size: { width: 120, height: 127 }},
-    { id: 13, position: { x: 268, y: 127 }, size: { width: 20, height: 50 }},
-    { id: 14, position: { x: 287, y: 147 }, size: { width: 70, height: 30 }},
-    { id: 15, position: { x: 387, y: 50 }, size: { width: 108, height: 50 }},
-    { id: 16, position: { x: 387, y: 99 }, size: { width: 30, height: 78 }},
-    { id: 17, position: { x: 416, y: 99 }, size: { width: 22, height: 58 }},
-    { id: 18, position: { x: 437, y: 99 }, size: { width: 22, height: 58 }},
-    { id: 19, position: { x: 458, y: 99 }, size: { width: 30, height: 58 }},
-    { id: 20, position: { x: 487, y: 99 }, size: { width: 8, height: 58 }},
-    { id: 21, position: { x: 416, y: 156 }, size: { width: 22, height: 21 }},
-    { id: 22, position: { x: 437, y: 156 }, size: { width: 22, height: 21 }},
-    { id: 23, position: { x: 458, y: 156 }, size: { width: 37, height: 21 }},
-  ]);
-  const [selectedRoomId, setSelectedRoomId] = useState(null);
-  const handleRoomSelect = (roomId) => {
-    setSelectedRoomId(roomId);
-  };
-
-  return (
-    <div className="directory">
-      <div style={{ display: "flex", flexDirection: "column"}}>
-        <FloorPlan rooms={rooms} selectedRoomId={selectedRoomId} onRoomSelect={handleRoomSelect} />
-        <RoomList rooms={rooms} onRoomSelect={handleRoomSelect} />
+    <div className="Directory">
+      <div className="Plan">
+        <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1CpjrqSU8ZoJJAdINTPPQi_Hf5Bk6IaE&ehbc=2E312F" width="100%" height="600" style={{ border: "0", marginTop: "-4px"}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" frameborder="0"></iframe>
+      </div>
+      <h1>TENANT DIRECTORY</h1>
+      <div className="Tenant-List">
+        <h2>FIRST FLOOR</h2>
+        <div className="List-Items">
+          <ul className="List-One">
+            <h3 className="font-bold">A</h3>
+            <li>A CRYSTALIZED MOMENT 1-498</li>
+            <li>ABEL MARCIAL 1-64</li>
+            <li>ALMEIDA GRANITE & MARBLE 0-3</li>
+            <li>AMY ARAUJO 1-1B</li>
+            <li>APEX FITNESS 1-1</li>
+            <h3 className="font-bold">B</h3>
+            <li>BALLOONTASTIC EVENTS 1-24</li>
+            <li>BATH GLASS SHOWER DOORS 1-73</li>
+            <li>BELO BUILDERS 1-3</li>
+            <li>BETH & LARRY KIRK STUDIO O-2</li>
+            <li>BREANA SPELLMAN 1-51A</li>
+            <li>BRIAN CALUSINE 1-2</li>
+            <li>BRIAN FERGUSON 1-27</li>
+            <li>BRITTANY CARVALHO 1-115A</li>
+            <h3 className="font-bold">C</h3>
+            <li>CALAMARI COURT INC. B-2</li>
+            <li>CELS AND SONS 1-29</li>
+            <li>CHRISTINA AMARAL 1-74</li>
+            <li>CONSTANTLY VARIED GEAR N-3</li>
+          </ul>
+          <ul className="List-Two List-Right">
+            <li>CROSSFIT NB/ERG FIT 1-75</li>
+            <h3 className="font-bold">D</h3>
+            <li>DAVID NELSON B-7</li>
+            <li>DEBRA GARABEDIAN 1-32</li>
+            <li>DIGITAL 2000 1-13</li>
+            <li>DO CO CAFE 1-55</li>
+            <h3 className="font-bold">E</h3>
+            <li>EVOLUTION DANCE 1-38</li>
+            <h3 className="font-bold">F</h3>
+            <li>FIERCE & SOPHISTICATED BOUTIQUE 1-35A</li>
+            <li>FINE LINE CONSTRUCTION 1-31</li>
+            <li>FINEST HOUR ATHLETICS 1-27</li>
+            <h3 className="font-bold">G</h3>
+            <li>GATES OF HOPE 1-15</li>
+            <h3 className="font-bold">H</h3>
+            <li>HEATHER & TIMOTHY CREED 1-53A</li>
+            <h3 className="font-bold">I</h3>
+            <li>INTARAWUT 1-45</li>
+            <h3 className="font-bold">J</h3>
+            <li>JOSH & STANLEY SYLVIA 1-6</li>
+            <li>JP IMPORTS B-3</li>
+          </ul>
+          <ul className="List-Three">
+            <li>JUSTIN MONTERO 1-56</li>
+            <h3 className="font-bold">K</h3>
+            <li>KILBURN OFFICE 1-62</li>
+            <li>KJ DESIGNS 1-43</li>
+            <h3 className="font-bold">L</h3>
+            <li>LAWRENCE JAMISON 1-49</li>
+            <li>LEONEL'S NET O-7</li>
+            <h3 className="font-bold">M</h3>
+            <li>MARC STEVENS & CHRISTINA SCHULTZ 1-17</li>
+            <li>MEGAN THERESE PHOTOGRAPHY 1-37A</li>
+            <h3 className="font-bold">N</h3>
+            <li>NEW BEDFORD ANTIQUES B-4</li>
+            <li>NOEL'S ANTIQUES 1-111</li>
+            <li>NUTEX N-1</li>
+            <h3 className="font-bold">O</h3>
+            <li>OUT OF THE BOX ARCHITECTURE 1-4</li>
+            <h3 className="font-bold">P</h3>
+            <li>PATRIO DeMIRANDA 1-22</li>
+            <li>PLUMBERRY LANE 1-41A</li>
+            <h3 className="font-bold">R</h3>
+            <li>RANDY BIGGERS CONSTUCTION O-5</li>
+          </ul>
+          <ul className="List-Four List-Right">
+            <li>RAY'S STRIPPING AND REFINISHING 1-20</li>
+            <li>REAL WORLD ART & LITERATURE 1-33</li>
+            <li>ROLAND MORGAN ANTIQUES 1-8/14/21</li>
+            <h3 className="font-bold">S</h3>
+            <li>SCOTT KRUGER 1-26</li>
+            <li>SHARON SOLOMON 1-1C</li>
+            <li>SIMPLY SIERRA JEWELRY 1-35B</li>
+            <li>SOUTHCOAST SOLE COLLECTOR 1-39B</li>
+            <li>SOUTHCOAST STORM I 1-54</li>
+            <li>ST EDITION COLLECTABLES 1-41B</li>
+            <li>STEWART TRANSPORTATION O-1</li>
+            <li>SUSAN MALONEY 1-25</li>
+            <li>SWWS PHOTOGRAPHY 1-34/41</li>
+            <h3 className="font-bold">T</h3>
+            <li>TERRY CORMIER B-5</li>
+            <li>TRIBE FITNESS 1-9</li>
+            <li>TUS TECHNOLOGIES 1-10</li>
+            <h3 className="font-bold">V</h3>
+            <li>VICEK DIZZY EAGLE TRADING CO. 1-5</li>
+          </ul>
+        </div>
+      </div>
+      <div className="Tenant-List pt-20">
+        <h2>SECOND FLOOR</h2>
+        <div className="List-Items">
+          <ul className="List-One">
+            <h3 className="font-bold">A</h3>
+            <li>A & M DANCE STUDIO 2-97</li>
+            <li>A PACKAGE 2-98</li>
+            <li>ALEXANDRA THOMPSON 2-2</li>
+            <li>ALMA CUMMINGS ART 2-74</li>
+            <li>ANA GRACA 2-108</li>
+            <li>ARTUR PEREIRA 2-44</li>
+            <h3 className="font-bold">B</h3>
+            <li>BASS REDDY ROOTER 2-11</li>
+            <li>BETTE CARNEY ART 2-105</li>
+            <h3 className="font-bold">C</h3>
+            <li>CHRIS & KATHLEEN SNOW 2-84</li>
+            <li>CIPRIANI CONSTRUCTION CONSULTING 2-58</li>
+            <li>CRABBE CHIROPRACTIC 2-104A</li>
+            <h3 className="font-bold">D</h3>
+            <li>DANIELLE MIO2ZA 2.12</li>
+            <li>D&J TRUCKING 2-116</li>
+            <h3 className="font-bold">E</h3>
+            <li>EVERETT & JANN MACOMBER 2-63</li>
+            <h3 className="font-bold">F</h3>
+            <li>FLIP THIS DOLL HOUSE 2-102</li>
+            <h3 className="font-bold">H</h3>
+            <li>HAILEY DUBE 2-40</li>
+            <li>HAOLOI 2-95</li>
+          </ul>
+          <ul className="List-Two List-Right">
+            <li>HARTMAN DEETZ 2-19</li>
+            <li>HARPER GR/WINTERS GR/SNAP DRAGONS 2-85</li>
+            <li>HIIT & FIT PERSONAL TRAINING 2-114</li>
+            <h3 className="font-bold">I</h3>
+            <li>INFINITE PROMISES 2-109</li>
+            <h3 className="font-bold">J</h3>
+            <li>JACQUELYNN PULLO/MARGARET BEESE 2-15</li>
+            <li>JADE BOND 2-49</li>
+            <li>JMAC STUDIO 2-89</li>
+            <li>JOHN JAMESON 2-24</li>
+            <li>JUDITH KLEIN 2-87</li>
+            <h3 className="font-bold">K</h3>
+            <li>KAREN ZUKAS ART 2-34</li>
+            <li>KAT KNUTSEN ART 2-71</li>
+            <li>KETURAH MITCHELL 2-78</li>
+            <li>KILBURN HOLISTIC GROUP 2-88</li>
+            <li>KYLE STAMMLER/CASSIDY FLOOD 2-18</li>
+            <h3 className="font-bold">L</h3>
+            <li>LAURI BOTELHO 2-92</li>
+            <li>LESLIE PACHECO 2-33</li>
+            <li>LINDSEY BISSONETTE 2-778</li>
+            <li>LYNNE GEORGE 2-77E</li>
+            <h3 className="font-bold">M</h3>
+          </ul>
+          <ul className="List-Three">
+            <li>MAITOFITZ ART GALLERY 2-62</li>
+            <li>MARUSHKA LASZLO 2-49</li>
+            <li>MADEMOISELLE COUTURE 2-61</li>
+            <li>MELISSA FARIAS 2-45</li>
+            <li>MICHAEL MCGUIRE 2-77A</li>
+            <li>MICHAEL PRAT 2-14</li>
+            <li>MEAGAN THOMAS MAYPOLE WEAVER$ 2-66</li>
+            <li>MY AUNTS AND ME 2-53</li>
+            <h3 className="font-bold">N</h3>
+            <li>NANCY NAGEL 2-67/82</li>
+            <li>NILSA GARCIA-REY ART 2-70</li>
+            <h3 className="font-bold">P</h3>
+            <li>PAUL OGUNDANA 2-18</li>
+            <li>PETER LOCKE 2-93</li>
+            <li>PIETRO CHESSA 2-80</li>
+            <h3 className="font-bold">R</h3>
+            <li>RAMON ROLDAN 2-72</li>
+            <li>RELAX MASSAGE BY ROCHELLE 2-99</li>
+            <li>ROBERT SPAVENTO ESQ 2-103</li>
+            <h3 className="font-bold">S</h3>
+            <li>SERVEDWELL 2-81</li>
+          </ul>
+          <ul className="List-Four List-Right">
+            <li>SHEILA OLIVIERA ART STUDIO 2-115</li>
+            <li>SHIMALA GUITARS 2-90</li>
+            <li>SOLSHINE YOGA 2-107</li>
+            <li>SPIRITUAL DESTINY 2/111</li>
+            <li>STAR GARDEN STUDIO 2-60</li>
+            <li>SUZANNE VOMER 2-79</li>
+            <li>SVETLANA BIVENS 2-47</li>
+            <h3 className="font-bold">T</h3>
+            <li>TAKE 2WOI 2-75</li>
+            <li>TAMAUIN BAUMGARTEN 2-1</li>
+            <li>TAYLOR KUTTNER 2-77C</li>
+            <li>THAIS DURA IMAGES 2-7</li>
+            <li>THE ART UNIVERSE 2-64</li>
+            <li>THERMOBIND 2-86</li>
+            <li>THOMAS ALVES 2-69</li>
+            <li>THOMAS GOYETTE 2-34</li>
+            <li>UTOPIA'S TEA SHOPPE 2-39/41</li>
+            <h3 className="font-bold">W</h3>
+            <li>WBJ CREATIONS 2-26</li>
+            <li>WOLF GEAR 2-21</li>
+            <li>WINSTON GALLERY 2-106</li>
+            <h3 className="font-bold">Y</h3>
+            <li>YELLOWBOX ENTERTAINMENT 2-113</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
-};
+}
 
 export default Directory;
